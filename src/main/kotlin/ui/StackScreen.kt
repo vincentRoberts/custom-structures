6f44ffe570447caf32c60d846573e34442bc87ce
vincentRoberts <20103510@mail.wit.ie>
2024-08-16 11:20:49 +0100

    Update in new StackScreen UI

--- src/main/kotlin/ui/StackScreen.kt
+++ src/main/kotlin/ui/StackScreen.kt
@@ -1,20 +1,55 @@
 package ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import datastructures.lifoStackOf
+import datastructures.LIFOStack
+import interfaces.Stack
 
 @Composable
 fun StackScreen() {
+
     Column(Modifier.padding(16.dp)) {
         Text(
             "Stack",
             style = MaterialTheme.typography.headlineLarge,
             color = MaterialTheme.colorScheme.primary
         )
+
+        val stackList = listOf(
+            lifoStackOf(1.0, 2.0, 3.0, 4.0),
+            LIFOStack<String>().apply { pushAll(setOf("MySQL", "SQLite", "Mongo")) }
+        )
+
+        LazyColumn(modifier = Modifier.weight(1f)) {
+            items(stackList) { stack ->
+                StackContent(stack = stack, stack.javaClass.simpleName)
+            }
+        }
+    }
+}
+
+@Composable
+fun StackContent(stack: Stack<*>, cardTitle: String) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+    ) {
+        Row (modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+            Text(text = "LIFO Stack", style = MaterialTheme.typography.bodyLarge)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row (modifier = Modifier.fillMaxWidth().padding(8.dp)){
+            Text(text = "Size: ${stack.size()}\n${stack}", modifier = Modifier.weight(1f))
+            Text(text = "Popped Element: ${stack.pop()}", modifier = Modifier.weight(1f))
+            Text(text = "Size: ${stack.size()}\n${stack}", modifier = Modifier.weight(1f))
+        }
     }
 }
