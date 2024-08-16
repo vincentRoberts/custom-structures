4cc62cbc2ce397003ce5b6c8729881d8fbbd08cd
vincentRoberts <20103510@mail.wit.ie>
2024-08-16 11:06:06 +0100

    Update in theme

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.compose.material3:material3-desktop:1.4.3")
 }
 
 compose.desktop {
