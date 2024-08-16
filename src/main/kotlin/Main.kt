0fd42f71e265e406f75442372ff28c7d8ed7427a
vincentRoberts <20103510@mail.wit.ie>
2024-08-16 11:09:10 +0100

    Update in StackScreen and Main.kt with material3 changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,24 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.example.compose.AppTheme
 import ui.StackScreen
 
 @Composable
 @Preview
 fun App() {
-
-    MaterialTheme {
-        StackScreen()
-    }
+    StackScreen()
 }
 
 fun main() = application {
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Custom Data Structures"
     ) {
-        App()
+        AppTheme (){
+            App()
+        }
     }
 }
--- src/main/kotlin/ui/StackScreen.kt
+++ src/main/kotlin/ui/StackScreen.kt
@@ -2,8 +2,8 @@ package ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -11,8 +11,10 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun StackScreen() {
     Column(Modifier.padding(16.dp)) {
-        Text("Stack",
-            style = MaterialTheme.typography.h4,
-            color = MaterialTheme.colors.primary)
+        Text(
+            "Stack",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
     }
 }
