3838f6e40a4973214da6ee9788f93027df840c53
vincentRoberts <20103510@mail.wit.ie>
2024-08-17 10:11:05 +0100

    FIFOStack created and LIFOStack updated with nestedClasses

--- src/test/kotlin/FIFOStackTest.kt
+++ src/test/kotlin/FIFOStackTest.kt
@@ -1,11 +1,11 @@
-import datastructures.LIFOStack
-import datastructures.lifoStackOf
+import datastructures.FIFOStack
+import datastructures.fifoStackOf
 import interfaces.Stack
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class LIFOStackTest {
+class FIFOStackTest {
 
     @Nested
     inner class PushTests {
@@ -13,7 +13,7 @@ class LIFOStackTest {
         @Test
         fun `push should add elements to the stack`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
 
 
             stack.push(1)
@@ -26,9 +26,9 @@ class LIFOStackTest {
         }
 
         @Test
-        fun `stack instantiated with items, when push is called, then it adds the element at the top`() {
+        fun `stack instantiated with items, when push is called, then it adds the element at the end`() {
 
-            val stack = lifoStackOf(1, 2, 3, 4, 5)
+            val stack = fifoStackOf(1, 2, 3, 4, 5)
             val element = 42
 
 
@@ -46,7 +46,7 @@ class LIFOStackTest {
         @Test
         fun `stack initialized by constructor push should add elements to the stack`() {
 
-            val stack = LIFOStack<Int>()
+            val stack = FIFOStack<Int>()
 
 
             stack.push(1)
@@ -61,7 +61,7 @@ class LIFOStackTest {
         @Test
         fun `pushAll for lists should add all elements to the stack`() {
 
-            val stack = lifoStackOf<String>()
+            val stack = fifoStackOf<String>()
             val items = listOf("Cat", "Dog", "Mouse", "Horse", "Giraffe")
 
 
@@ -75,7 +75,7 @@ class LIFOStackTest {
         @Test
         fun `pushAll for sets should add all elements to the stack`() {
 
-            val stack = lifoStackOf<String>()
+            val stack = fifoStackOf<String>()
             val items = setOf("Cat", "Dog", "Mouse", "Horse", "Giraffe")
 
 
@@ -91,9 +91,9 @@ class LIFOStackTest {
     inner class PopTests {
 
         @Test
-        fun `pop should remove and return the top element from the stack`() {
+        fun `pop should remove and return the first element from the stack`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
             stack.push(1)
             stack.push(2)
 
@@ -101,14 +101,14 @@ class LIFOStackTest {
             val poppedElement = stack.pop()
 
 
-            assertEquals(2, poppedElement)
+            assertEquals(1, poppedElement)
             assertEquals(1, stack.size())
         }
 
         @Test
         fun `pop on an empty stack should return null`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
 
 
             val poppedElement = stack.pop()
@@ -119,9 +119,9 @@ class LIFOStackTest {
         }
 
         @Test
-        fun `popAll should remove and return all elements from the stack in reverse order`() {
+        fun `popAll should remove and return all elements from the stack in FIFO order`() {
 
-            val stack: Stack<Int> = LIFOStack()
+            val stack: Stack<Int> = FIFOStack()
             stack.push(1)
             stack.push(2)
             stack.push(3)
@@ -130,7 +130,7 @@ class LIFOStackTest {
             val poppedElements = stack.popAll()
 
 
-            assertEquals(listOf(3, 2, 1), poppedElements)
+            assertEquals(listOf(1, 2, 3), poppedElements)
             assertTrue(stack.isEmpty())
         }
     }
@@ -139,9 +139,9 @@ class LIFOStackTest {
     inner class PeekTests {
 
         @Test
-        fun `peek should return the top element from the stack without removing it`() {
+        fun `peek should return the first element from the stack without removing it`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
             stack.push(1)
             stack.push(2)
 
@@ -149,14 +149,14 @@ class LIFOStackTest {
             val topElement = stack.peek()
 
 
-            assertEquals(2, topElement)
+            assertEquals(1, topElement)
             assertEquals(2, stack.size())
         }
 
         @Test
         fun `peek on an empty stack should return null`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
 
 
             val topElement = stack.peek()
@@ -173,7 +173,7 @@ class LIFOStackTest {
         @Test
         fun `size should return the number of elements in the stack`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
 
 
             assertEquals(0, stack.size())
@@ -188,7 +188,7 @@ class LIFOStackTest {
         @Test
         fun `isEmpty should return true if the stack is empty`() {
 
-            val stack = lifoStackOf<Int>()
+            val stack = fifoStackOf<Int>()
 
 
             assertTrue(stack.isEmpty())
