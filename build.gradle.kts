d12df72d83f234b5188cf939187f1faf4dd11be7
vincentRoberts <20103510@mail.wit.ie>
2024-08-17 10:21:25 +0100

    Updates to DOkka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
+    kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.compose") version "1.6.0"
+    id("org.jetbrains.dokka") version "1.8.20"
 }
 
 group = "ie.setu"
@@ -14,14 +15,14 @@ repositories {
     google()
 }
 
-dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs)
-    implementation("org.jetbrains.compose.material3:material3-desktop:1.4.3")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+
+    dependencies {
+        implementation(compose.desktop.currentOs)
+        implementation("org.jetbrains.compose.material3:material3-desktop:1.4.3")
+        implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    }
 }
 
 compose.desktop {
