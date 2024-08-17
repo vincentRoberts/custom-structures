e5222294d74b5de26e825c0e80a4ae120ce852c9
vincentRoberts <20103510@mail.wit.ie>
2024-08-17 10:39:55 +0100

    Reverted to pre-Dokka gradle due to androidx errors that I couldnt fix

---
+++ src/main/kotlin/ui/QueueScreen.kt
@@ -0,0 +1,22 @@
+package ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun QueueScreen() {
+
+    Column(Modifier.padding(16.dp))
+    {
+        Text(
+            "Queue",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
